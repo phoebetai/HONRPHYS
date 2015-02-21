bf0edd01b10bd24d366c8ff3239658cfd09397a7
Sarah <eno@umd.edu>
2015-02-21 18:34:19 -0500

    haha

--- main.cpp
+++ main.cpp
@@ -4,5 +4,18 @@ using namespace std;
 
 int main () {
   cout <<"hello world !\n";
+
+  int i=2;
+  cout<<"i = "<<i<<endl;
+
+  double a=3.3;
+  cout<<"a = "<<a<<endl;
+
+  cout<<"a * i ="<<a*i<<endl;
+
+  int j=a*i;
+  
+  cout<<"a*i is "<<j<<endl;
+
   return 0;
 }
